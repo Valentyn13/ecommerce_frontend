bd86d36ab07ed29bba9373d285c1b78f2fbb3c8f
Valentyn13 <valentyn13git@gmail.com>
2023-09-29 20:37:45 +0300

    create burger menu

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -16,7 +16,10 @@ import { FaScaleUnbalanced } from "react-icons/fa6";
 import { FiLogIn as LoginIcon } from "react-icons/fi";
 import { HiOutlineViewGrid as CatalogIcon } from "react-icons/hi";
 import { BsPersonCircle as ProfileIcon } from "react-icons/bs";
-
+import {
+  RxHamburgerMenu as BurgerButton,
+  RxCross1 as CloseBurger,
+} from "react-icons/rx";
 import "./Header.scss";
 
 const Header = () => {
@@ -35,7 +38,7 @@ const Header = () => {
   const [isCartActive, setIsCartActive] = useState(false);
   const [isFavouritesActive, setIsFavouritesActive] = useState(false);
   const [isCompareModalActive, setIsCompareModalActive] = useState(false);
-
+  const [isBurgerActive, setIsBurgerActive] = useState(false);
   const logoutHandler: MouseEventHandler<HTMLButtonElement> = () => {
     dispatch(clearCart());
     dispatch(logout());
@@ -45,81 +48,120 @@ const Header = () => {
   return (
     <header className="header">
       <div className="header__container _container">
-        <div className="header__catalog catalog" onClick={() => {navigate('/')}}>
-          <div className="catalog__icon">
-            <CatalogIcon />
-          </div>
-          <h3 className="catalog__text">Catalog</h3>
-          <div className="catalog__arrow">
-            <CatalogArrow />
-          </div>
+        <div className="burger-button">
+          <BurgerButton onClick={() => setIsBurgerActive(true)} />
         </div>
-        <div className="header__options">
-          {user && (
-            <div className="menu-wrap">
-              <ul className="menu">
-                <li className="menu-item">
-                  <div className="header__user">
-                    <div className="header__user_logo">
-                      <ProfileIcon />
-                    </div>
-                    <div className="header__user_email">{user.user.email}</div>
-                  </div>
-                  <ul className="drop-menu">
-                    <li className="drop-menu-item">
-                      <Link to="/profile">Profile</Link>
-                    </li>
-                    <li className="drop-menu-item">
-                      <button
-                        onClick={logoutHandler}
-                        style={{ color: "red", backgroundColor: "transparent" }}
-                      >
-                        Logout
-                      </button>
-                    </li>
-                  </ul>
-                </li>
-              </ul>
+        <div
+          className={
+            isBurgerActive ? "burger-wrapper burger-active" : "burger-wrapper"
+          }
+        >
+          <div
+            className="header__catalog catalog"
+            onClick={() => {
+              navigate("/");
+            }}
+          >
+            <div className="catalog__icon">
+              <CatalogIcon />
             </div>
-          )}
-          <div className="header__options__hw-icons">
-            <div onClick={() => setIsFavouritesActive(true)}>
-              <MdFavorite />
-              {favourites.length > 0 && (
-                <div className="hw-icons__count">{favourites.length}</div>
-              )}
-            </div>
-            <div onClick={() => setIsCompareModalActive(true)}>
-              <FaScaleUnbalanced />
-              {compareList.length > 0 && (
-                <div className="hw-icons__count">{compareList.length}</div>
-              )}
+            <h3 className="catalog__text">Catalog</h3>
+            <div className="catalog__arrow">
+              <CatalogArrow />
             </div>
           </div>
-          {!user && (
-            <div className="header__dropdown">
-              <div
-                className="header__auth"
-                onClick={() => navigate("/register")}
-              >
-                <h4>Sign Up</h4>
-                <div>
-                  <LoginIcon />
-                </div>
+          <div className="header__options">
+            <div
+              className="close-burger"
+              onClick={() => setIsBurgerActive(false)}
+            >
+              <CloseBurger />
+            </div>
+            {user && (
+              <div className="menu-wrap">
+                <ul className="menu">
+                  <li className="menu-item">
+                    <div className="header__user">
+                      <div className="header__user_logo">
+                        <ProfileIcon />
+                      </div>
+                      <div className="header__user_email">
+                        {user.user.email}
+                      </div>
+                    </div>
+                    <ul className="drop-menu">
+                      <li
+                        className="drop-menu-item"
+                        onClick={() => {
+                          setIsBurgerActive(false);
+                        }}
+                      >
+                        <Link to="/profile">Profile</Link>
+                      </li>
+                      <li className="drop-menu-item">
+                        <button
+                          onClick={(e) => {
+                            setIsBurgerActive(false);
+                            logoutHandler(e);
+                          }}
+                          style={{
+                            color: "red",
+                            backgroundColor: "transparent",
+                          }}
+                        >
+                          Logout
+                        </button>
+                      </li>
+                    </ul>
+                  </li>
+                </ul>
+              </div>
+            )}
+            <div className="header__options__hw-icons">
+              <div onClick={() => setIsFavouritesActive(true)}>
+                <MdFavorite />
+                {favourites.length > 0 && (
+                  <div className="hw-icons__count">{favourites.length}</div>
+                )}
+              </div>
+              <div onClick={() => setIsCompareModalActive(true)}>
+                <FaScaleUnbalanced />
+                {compareList.length > 0 && (
+                  <div className="hw-icons__count">{compareList.length}</div>
+                )}
               </div>
             </div>
-          )}
-          <div className="header__cart cart">
-            <div style={{ position: "relative" }}>
-              <ShopingCart
-                style={{ cursor: "pointer" }}
-                onClick={() => setIsCartActive(true)}
-              />
-              {cartItems.length > 0 && (
-                <div className="cart__item_count"> {cartItems.length}</div>
-              )}
+            {!user && (
+              <div className="header__dropdown">
+                <div
+                  className="header__auth"
+                  onClick={() => {
+                    setIsBurgerActive(false);
+                    navigate("/register");
+                  }}
+                >
+                  <h4>Sign Up</h4>
+                  <div>
+                    <LoginIcon />
+                  </div>
+                </div>
+              </div>
+            )}
+            <div className="header__cart cart">
+              <div style={{ position: "relative" }}>
+                <ShopingCart
+                  style={{ cursor: "pointer" }}
+                  onClick={() => {
+                    setIsBurgerActive(false);
+                    setIsCartActive(true);
+                  }}
+                />
+                {cartItems.length > 0 && (
+                  <div className="cart__item_count"> {cartItems.length}</div>
+                )}
+              </div>
+              <h4 className="cart__text">Cart</h4>
             </div>
-            <h4 className="cart__text">Cart</h4>
           </div>
         </div>
       </div>

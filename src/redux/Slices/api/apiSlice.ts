9438996e9b9c3e7bd08022417369a625f11b29cc
Valentyn13 <valentyn13git@gmail.com>
2023-10-06 18:24:36 +0300

    change baseurl

--- src/redux/Slices/api/apiSlice.ts
+++ src/redux/Slices/api/apiSlice.ts
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const baseQuery = fetchBaseQuery({ baseUrl: "http://localhost:4200" });
+const baseQuery = fetchBaseQuery({ baseUrl: "https://ecommerce-backend-kzo1yzxlr-valentyn13.vercel.app/" });
 
 export const apiSlice = createApi({
     baseQuery,
